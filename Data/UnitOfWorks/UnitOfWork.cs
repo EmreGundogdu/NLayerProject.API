0752c1f1fb7af41d4075e884b1b148b5d5fb7d22
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-04-24 12:52:30 +0300

    Connection String, Startup.cs updated

---
+++ Data/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using Core.Repositories;
+using Core.UnitOfWorks;
+using Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+        private ProductRepository _productRepository;
+        private CategoryRepository _categoryRepository;
+        public IProductRepository Product => _productRepository = _productRepository ?? new ProductRepository(_context);
+
+        public ICategoryRepository categories => _categoryRepository = _categoryRepository ?? new CategoryRepository(_context);
+        public UnitOfWork(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+        }
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
--- NLayerProject.API/Startup.cs
+++ NLayerProject.API/Startup.cs
@@ -1,7 +1,11 @@
+using Core.UnitOfWorks;
+using Data;
+using Data.UnitOfWorks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +29,11 @@ namespace NLayerProject.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration["ConnectionStrings:SqlConStr"].ToString());
+            });
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddControllers();
         }
 
