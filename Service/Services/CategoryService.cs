caf7e4499463af8ac5986472ea794ee7b9552a97
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-04-25 12:57:42 +0300

    Service.cs

--- Service/Services/CategoryService.cs
+++ Service/Services/CategoryService.cs
@@ -1,10 +1,23 @@
-﻿using System;
+﻿using Core.Models;
+using Core.Repositories;
+using Core.Services;
+using Core.UnitOfWorks;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Service.Services
 {
-    public class CategoryService
+    public class CategoryService : Service<Category>, ICategoryService
     {
+        public CategoryService(IUnitOfWork unitOfWork, IRepositoryGeneric<Category> repository) : base(unitOfWork, repository)
+        {
+        }
+
+        public Task<Category> GetWithProductsByIdAsync(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Service/Services/ProductService.cs
+++ Service/Services/ProductService.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Models;
+using Core.Repositories;
 using Core.Services;
 using Core.UnitOfWorks;
 using System;
@@ -9,69 +10,15 @@ using System.Threading.Tasks;
 
 namespace Service.Services
 {
-    public class ProductService : IProductService
+    public class ProductService : Service<Product>, IProductService
     {
-        private readonly IUnitOfWork _unitOfWork;
-        public ProductService(IUnitOfWork unitOfWork)
+        public ProductService(IUnitOfWork unitOfWork, IRepositoryGeneric<Product> repository) : base(unitOfWork, repository)
         {
-            _unitOfWork = unitOfWork;
-        }
-        public async Task<Product> AddAsync(Product entity)
-        {
-            await _unitOfWork.Product.AddAsync(entity);
-            await _unitOfWork.CommitAsync();
-            return entity;
         }
 
-        public async Task<IEnumerable<Product>> AddRangeAsync(IEnumerable<Product> entities)
+        public Task<Product> GetWithCategoryByIdAsync(int productId)
         {
-            await _unitOfWork.Product.AddRangeAsync(entities);
-            await _unitOfWork.CommitAsync();
-            return entities;
-        }
-
-        public async Task<IEnumerable<Product>> Where(Expression<Func<Product, bool>> predicate)
-        {
-            return await _unitOfWork.Product.Where(predicate);
-        }
-
-        public async Task<IEnumerable<Product>> GetAllAsync()
-        {
-            return await _unitOfWork.Product.GetAllAsync();
-        }
-
-        public async Task<Product> GetByIdAsync(int id)
-        {
-            return await _unitOfWork.Product.GetByIdAsync(id);
-        }
-
-        public async Task<Product> GetWithCategoryByIdAsync(int productId)
-        {
-            return await _unitOfWork.Product.GetWithCategoryByIdAsync(productId);
-        }
-
-        public void Remove(Product entity)
-        {
-            _unitOfWork.Product.Remove(entity);
-            _unitOfWork.Commit();
-        }
-
-        public void RemoveRange(IEnumerable<Product> entities)
-        {
-            _unitOfWork.Product.RemoveRange(entities);
-            _unitOfWork.Commit();
-        }
-
-        public async Task<Product> SingleOrDefaultAsync(Expression<Func<Product, bool>> predicate)
-        {
-            return await _unitOfWork.Product.SingleOrDefaultAsync(predicate);
-        }
-
-        public Product Update(Product entity)
-        {
-            var updateProduct = _unitOfWork.Product.Update(entity);
-            _unitOfWork.Commit();
-            return updateProduct;
+            throw new NotImplementedException();
         }
     }
 }
