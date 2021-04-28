e5b7669154b1769f93efb1327f4633829e8d68df
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-04-28 14:08:35 +0300

    Person model added, configuration

---
+++ Data/Configurations/PersonConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Configurations
+{
+    public class PersonConfiguration : IEntityTypeConfiguration<Person>
+    {
+        public void Configure(EntityTypeBuilder<Person> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+            builder.Property(x => x.Name).HasMaxLength(50);
+            builder.Property(x => x.Surname).HasMaxLength(50);
+        }
+    }
+}
