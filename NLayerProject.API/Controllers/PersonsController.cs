adaede1ba33961524b651a7040daa130ad098860
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-04-28 14:15:46 +0300

    Person Controller

---
+++ NLayerProject.API/Controllers/PersonsController.cs
@@ -0,0 +1,28 @@
+﻿using Core.Models;
+using Core.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NLayerProject.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+        private readonly IService<Person> _personService;
+        public PersonsController(IService<Person> service)
+        {
+            _personService = service;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var person = await _personService.GetAllAsync();
+            return Ok(person);
+        }
+    }
+}
